382eb71fa63eb66922c0b2fa8635b217a8b4723e
Justforvan <justforvangroup@gmail.com>
2023-10-28 16:36:20 +0800

    Remaking the FAQ page

--- lib/pages/facts.dart
+++ lib/pages/facts.dart
@@ -2,8 +2,89 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:accordion/accordion.dart';
 import 'package:penisure/data/faq_data.dart';
+
+class AccordionWidget extends StatefulWidget {
+  final String title;
+  final Widget content;
+
+  const AccordionWidget({
+    super.key,
+    required this.title,
+    required this.content,
+  });
+
+  @override
+  _AccordionWidgetState createState() => _AccordionWidgetState();
+}
+
+class _AccordionWidgetState extends State<AccordionWidget> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[900],
+                borderRadius: BorderRadius.vertical(
+                  top: const Radius.circular(8),
+                  bottom: Radius.circular(isExpanded ? 0 : 8),
+                ),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      widget.title,
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    isExpanded
+                        ? Icons.keyboard_arrow_up
+                        : Icons.keyboard_arrow_down,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          if (isExpanded)
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                borderRadius: const BorderRadius.vertical(
+                  bottom: Radius.circular(8),
+                ),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: widget.content,
+            ),
+          const SizedBox(height: 8), // Add spacing between sections
+        ],
+      ),
+    );
+  }
+}
+
+
 
 class FaQPage extends StatefulWidget {
   const FaQPage({Key? key}) : super(key: key);
@@ -13,7 +94,7 @@ class FaQPage extends StatefulWidget {
 }
 
 class _FaQPageState extends State<FaQPage> {
-  late Assets faqData;
+  Assets? faqData;
 
   @override
   void initState() {
@@ -22,7 +103,8 @@ class _FaQPageState extends State<FaQPage> {
   }
 
   Future<void> loadFaqData() async {
-    final String faqDataJson = await rootBundle.loadString('assets/faq_data.json');
+    final String faqDataJson =
+    await rootBundle.loadString('assets/faq_data.json');
     final decodedData = json.decode(faqDataJson);
     setState(() {
       faqData = Assets.fromJson(decodedData);
@@ -35,7 +117,7 @@ class _FaQPageState extends State<FaQPage> {
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
-          backgroundColor: Colors.blue[900],
+          backgroundColor: Colors.green[900],
           title: const Text('Facts about your penis'),
         ),
         body: SingleChildScrollView(
@@ -44,32 +126,73 @@ class _FaQPageState extends State<FaQPage> {
               const Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Text(
-                  'Your penis is unique. Here are some facts about it',
                   style: TextStyle(fontSize: 18),
+                  'Your penis is an incredibly unique and fascinating part of your body. There \'s so much more to it than meets the eye. Let \'s explore some intriguing facts and insights that will help you appreciate its complexity and function like never before. Join us on this enlightening journey into the world of this distinctive and important organ.',
+                  textAlign: TextAlign.center,
                 ),
               ),
-              if (faqData != null)
-                Accordion(
-                  scaleWhenAnimating: false,
-                  children: [
-                    AccordionSection(
-                      header: Text(faqData.questions.q1,
-                          style: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold)),
-                      headerBorderRadius: 4,
-                      headerPadding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                      headerBackgroundColor: Colors.blue[900],
-                      content: Text(faqData.answers.a1),
-                      contentBorderRadius: 4,
-                    ),
-                    // Repeat for other sections
-                  ],
-                ),
+              if (faqData != null) ...buildAccordionWidgets(),
             ],
           ),
         ),
       ),
     );
+  }
+
+  List<AccordionWidget> buildAccordionWidgets() {
+    return [
+      AccordionWidget(
+        title: faqData!.questions.q1,
+        content: Text(faqData!.answers.a1),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q2,
+        content: Text(faqData!.answers.a2),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q3,
+        content: Text(faqData!.answers.a3),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q4,
+        content: Text(faqData!.answers.a4),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q5,
+        content: Text(faqData!.answers.a5),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q6,
+        content: Text(faqData!.answers.a6),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q7,
+        content: Text(faqData!.answers.a7),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q8,
+        content: Text(faqData!.answers.a8),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q9,
+        content: Text(faqData!.answers.a9),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q10,
+        content: Text(faqData!.answers.a10),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q11,
+        content: Text(faqData!.answers.a11),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q12,
+        content: Text(faqData!.answers.a12),
+      ),
+      AccordionWidget(
+        title: faqData!.questions.q13,
+        content: Text(faqData!.answers.a13),
+      ),
+    ];
   }
 }
